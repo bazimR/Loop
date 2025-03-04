66d7159e6e92acb786f80e8dafb0b8918b5627c6
bazimR <rishaalmuhammed@gmail.com>
2025-03-04 17:53:14 +0530

    added new add screen,obserbale state and nav class

---
+++ Loop/AddHabit.swift
@@ -0,0 +1,18 @@
+//
+//  AddHabit.swift
+//  Loop
+//
+//  Created by Rishal Bazim on 04/03/25.
+//
+
+import SwiftUI
+
+struct AddHabit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddHabit()
+}
---
+++ Loop/HabitType.swift
@@ -0,0 +1,8 @@
+//
+//  HabitType.swift
+//  Loop
+//
+//  Created by Rishal Bazim on 04/03/25.
+//
+
+import Foundation
